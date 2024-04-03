14ab59e6d5e1adb786955869b44ea70241eb43ec
Garbi93 <iseung2093@gmail.com>
2024-04-03 16:32:56 +0900

    04/03 시큐리티 프로젝트 연습 백엔드 초기 셋팅

---
+++ src/main/java/com/example/securityprjpracticeback/SecurityPrjPracticeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityprjpracticeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPrjPracticeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityPrjPracticeBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securityprjpracticeback/SecurityPrjPracticeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityprjpracticeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPrjPracticeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
