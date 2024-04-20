90f33fd31645ff654fb4118b98cf05079003402c
Garbi93 <iseung2093@gmail.com>
2024-04-20 16:47:22 +0900

    4/20 제품 엔티티와 제품 이미지 엔티티를 생성 하여 이미지 엔티티 클래스를 제품 클래스에 넣어서 관리, 제품 테스트 코드 작성 후 제품 생성 테스트

---
+++ src/main/java/com/example/securityprjpracticeback/domain/Product.java
@@ -0,0 +1,71 @@
+package com.example.securityprjpracticeback.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "tbl_product")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "imageList")
+public class Product {
+
+    // 상품 id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pno;
+
+    // 상품명
+    private String pname;
+
+    // 상품 가격
+    private int price;
+
+    // 상품 설명
+    private String pdesc;
+
+    // 상품 삭제 상태
+    private boolean delFlag;
+
+    // 상품 이미지
+    @ElementCollection
+    @Builder.Default
+    private List<ProductImage> imageList = new ArrayList<>();
+
+    public void changePrice(int price) {
+        this.price = price;
+    }
+
+    public void changeDesc(String desc) {
+        this.pdesc = desc;
+    }
+
+    public void changeName(String name) {
+        this.pname = name;
+    }
+
+    public void addImage(ProductImage image) {
+        image.setOrd(imageList.size());
+        imageList.add(image);
+
+    }
+
+    public void addImageString(String fileName) {
+
+        ProductImage productImage = ProductImage.builder()
+                .fileName(fileName)
+                .build();
+
+        addImage(productImage);
+    }
+
+    public void clearList() {
+
+        this.imageList.clear();
+    }
+}
