d750291664e8b01fe6b5362a2c5bf96bcbcc29cf
Garbi93 <iseung2093@gmail.com>
2024-04-20 14:53:21 +0900

    4/20 파일 업로드랑 thumbnailator 로 큰 이미지 파일 축소해서 만들기

--- src/main/java/com/example/securityprjpracticeback/util/CustomFileUtil.java
+++ src/main/java/com/example/securityprjpracticeback/util/CustomFileUtil.java
@@ -3,6 +3,7 @@ package com.example.securityprjpracticeback.util;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -51,8 +52,23 @@ public class CustomFileUtil {
             Path savePath = Paths.get(uploadPath, savedName);
 
             try {
-                Files.copy(file.getInputStream(), savePath);
+                Files.copy(file.getInputStream(), savePath); // 원본 파일 업로드
+
+                // 파일이 이미지 파일일때만 썸네일 이미지로 만들어 주기
+                String contentType = file.getContentType(); // Mim type 이라고 나온다.
+
+                // 컨텐트 타입이 안 비어 있고 파일형식이 image 로 시작 한다면
+                if (contentType != null || contentType.startsWith("image")) {
+                    Path thumbnailPath = Paths.get(uploadPath, "s_" + savedName);
+
+                    Thumbnails.of(savePath.toFile()).size(200, 200).toFile(thumbnailPath.toFile());
+
+                }
+
+
+
                 uploadNames.add(savedName);
+
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
