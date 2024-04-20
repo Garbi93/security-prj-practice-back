82970605fb1559c36d7db50e3aa76af2810117e2
Garbi93 <iseung2093@gmail.com>
2024-04-20 15:32:07 +0900

    4/20 파일 조회, 삭제 컴포넌트 생성 -> 조회 기능 작동 컨트롤러 추가

--- src/main/java/com/example/securityprjpracticeback/util/CustomFileUtil.java
+++ src/main/java/com/example/securityprjpracticeback/util/CustomFileUtil.java
@@ -5,6 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+// 파일 관련 공통 기능
 @Component
 @Log4j2
 @RequiredArgsConstructor
@@ -25,6 +30,7 @@ public class CustomFileUtil {
     @Value("${org.zerock.upload.path}")
     private String uploadPath;
 
+    // 최초 폴더 생성
     @PostConstruct
     public void init() {
         File tempFolder = new File(uploadPath);
@@ -38,6 +44,7 @@ public class CustomFileUtil {
         log.info(uploadPath);
     }
 
+    // 파일 저장
     public List<String> saveFiles(List<MultipartFile> files) throws RuntimeException {
 
         if (files == null || files.size() == 0) {
@@ -76,5 +83,49 @@ public class CustomFileUtil {
         
         return uploadNames;
         
+    }
+
+    // 파일 조회
+    public ResponseEntity<Resource> getFile(String fileName) {
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+        if (!resource.isReadable()) {
+            resource = new FileSystemResource(uploadPath + File.separator + "default.jpeg");
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+
+        try {
+            headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
+    // 파일 삭제
+    public void deleteFiles(List<String> fileNames) {
+
+        // 파일 이름이 없거나 파일이 없으면 중지
+        if (fileNames == null || fileNames.isEmpty()) {
+            return;
+        }
+
+        fileNames.forEach(fileName ->{
+            // 썸네일 삭제
+            String thumbnailFileName = "s_" + fileName;
+
+            Path thumbnailPath = Paths.get(uploadPath, thumbnailFileName);
+            Path filePath = Paths.get(uploadPath, fileName);
+
+            try{
+                Files.deleteIfExists(filePath);
+                Files.deleteIfExists(thumbnailPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        });
+
     }
 }
