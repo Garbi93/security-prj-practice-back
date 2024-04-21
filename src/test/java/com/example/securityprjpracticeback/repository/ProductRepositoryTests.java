d5cc5096df7f15d9faa0c42abc62912565961c70
Garbi93 <iseung2093@gmail.com>
2024-04-21 22:35:04 +0900

    4/21 상품 조회, 상품 삭제 상태, 상품 업데이트 기능 추가 후 테스트 코드로 실행

--- src/test/java/com/example/securityprjpracticeback/repository/ProductRepositoryTests.java
+++ src/test/java/com/example/securityprjpracticeback/repository/ProductRepositoryTests.java
@@ -5,7 +5,10 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
@@ -24,5 +27,59 @@ public class ProductRepositoryTests {
         product.addImageString(UUID.randomUUID() + "_" + "IMAGE2.jpg");
 
         productRepository.save(product);
+    }
+
+    @Test
+    public void testRead() {
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.findById(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+
+        log.info(product.getImageList());
+    }
+
+    @Test
+    public void testRead2() {
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.selectOne(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+
+        log.info(product.getImageList());
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDelete() {
+
+        Long pno = 2L;
+
+        productRepository.updateToDelete(2L, true);
+    }
+
+    @Test
+    public void testUpdate() {
+        Product product = productRepository.selectOne(1L).get();
+
+        product.changePrice(3000);
+
+        product.clearList();
+
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE1.jpg");
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE2.jpg");
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE3.jpg");
+
+        productRepository.save(product);
+
     }
 }
