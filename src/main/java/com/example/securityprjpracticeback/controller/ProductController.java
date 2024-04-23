ad0b407de4488e5c0edb06e3c0f6a98372aca3d4
Garbi93 <iseung2093@gmail.com>
2024-04-23 16:49:54 +0900

    4/23 ProductController 의 상품 등록인 PostMapping 부분에 딜레이 1.5초 넣어줌

--- src/main/java/com/example/securityprjpracticeback/controller/ProductController.java
+++ src/main/java/com/example/securityprjpracticeback/controller/ProductController.java
@@ -66,6 +66,12 @@ public class ProductController {
 
         Long pno = productService.register(productDTO);
 
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         return Map.of("result", pno);
     }
 
